8e41ab098f73db279fdb20690f6f967be0d4dcfa
Anjandev Momi <anjan@momi.ca>
2025-08-06 22:27:23 -0700

    test: add check_arch tests

--- test/helpers/test_package.py
+++ test/helpers/test_package.py
@@ -1,6 +1,8 @@
 # Copyright 2025 Anjandev Momi
 # SPDX-License-Identifier: GPL-3.0-or-later
-from pmb.helpers.package import remove_operators
+import pytest
+from pmb.helpers.package import remove_operators, check_arch
+from pmb.core.arch import Arch
 
 
 def test_remove_operators():
@@ -13,3 +15,12 @@ def test_remove_operators():
     assert remove_operators("sxmo-utils=10.0.0") == "sxmo-utils"
     assert remove_operators("sxmo-utils<10.0.0") == "sxmo-utils"
     assert remove_operators("sxmo-utils~10.0.0") == "sxmo-utils"
+
+
+def test_check_arch(pmaports):
+    with pytest.raises(RuntimeError):
+        check_arch("this-package-does-not-exist", Arch.aarch64)
+
+    assert check_arch("hello-world", Arch.aarch64)
+
+    assert not check_arch("linux-postmarketos-qcom-sdm845", Arch.x86_64)
