9ea18d18a15e9062b39ca2d72c959bd51b15429e
Casey Connolly <kcxt@postmarketos.org>
2026-07-11 20:26:41 +0900

    sideload: automatically sideload relevant subpackages
    Make sideload smarter by fetching the list of packages already
    installed on the remote and automatically sideloading subpackages of the
    specified package if they are already installed. This catch most obvious
    cases like -systemd subpackages which otherwise might get skipped or
    potentially uninstalled if the version numbers no longer match.
    
    Also use the list of installed packages to run with --no-network in more
    cases by checking if the dependencies for the packages being sideloaded
    are already installed.
    
    Signed-off-by: Casey Connolly <kcxt@postmarketos.org>

--- pmb/parse/arguments.py
+++ pmb/parse/arguments.py
@@ -283,6 +283,12 @@ def arguments_sideload(subparser: argparse._SubParsersAction) -> argparse.Argume
         action="store_true",
         dest="install_key",
     )
+    ret.add_argument(
+        "--strict",
+        help="Don't automatically sideload relevant subpackages or check network connectivity",
+        action="store_true",
+        dest="strict",
+    )
     return ret
 
 
